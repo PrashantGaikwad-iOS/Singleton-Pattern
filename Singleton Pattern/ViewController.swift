85dc2137407bf81bcfc30f82a41fc4cd674ab30e
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-21 22:57:30 +0530

    Better way of writing Singleton class

--- Singleton Pattern/ViewController.swift
+++ Singleton Pattern/ViewController.swift
@@ -13,8 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //In a single line you can access easily
+        LocationManager.shared.requestForLocation() // "Location granted"
+        //Access variable value
+        print(LocationManager.shared.locationGranted ?? false) // true
+        
     }
-
-
 }
 
+class LocationManager{
+    
+    static let shared = LocationManager()
+    
+    var locationGranted: Bool?
+    //Initializer access level change now
+    private init(){}
+    
+    func requestForLocation(){
+        //Code Process
+        locationGranted = true
+        print("Location granted")
+    }
+    
+}
